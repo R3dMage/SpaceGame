dbf0bf50d4b98b7a8ea7aa85a2f34e9fcef9dfe6
Jonathan <jonr79@hotmail.com>
2023-12-19 20:26:12 -0500

    Create little star objects for the background

--- script/background.js
+++ script/background.js
@@ -5,40 +5,49 @@ background = function(numberOfCircles, height, width){
 	this.howManyCircles = numberOfCircles;
 	this.circles = [];
 		
-	this.initCircles = function(){
+	this.initialize = function(){
 		for(let i = 0; i < this.howManyCircles; i++)
-			this.circles.push([Math.random() * width, Math.random() * height,
-			Math.random() * 100, Math.random() / 2, Math.random() * 10]);
-		//add information about circles into  
-		//the 'circles' Array. It is x & y positions,   
-		//radius from 0-100 and transparency   
-		//from 0-0.5 (0 is invisible, 1 no transparency)
+			this.circles.push(
+			new star(Math.random() * this.width, Math.random() * this.height, Math.random() * 5, this.getTransparency(), Math.random() * 10));
 	}
 
-	this.drawCircles = function(ctx){
+	this.draw = function(ctx){
 		for (let i = 0; i < this.howManyCircles; i++){
-			ctx.fillStyle = 'rgba(255, 255, 255, ' + this.circles[i][3] + ')';
-			ctx.beginPath();
-			ctx.arc(this.circles[i][0], this.circles[i][1], this.circles[i][2], 0, Math.PI * 2, true);
-			// arc(x, y, radius, startAngle, endAngle, anticlockwise)
-			// circle always has PI*2 end angle
-			
-			ctx.closePath();
-			ctx.fill();
+			this.circles[i].draw(ctx);
 		}
 	}
 
-	this.moveCircles = function(deltaY){
+	this.move = function(deltaY){
 		for (let i = 0; i < this.howManyCircles; i++){
-			if (this.circles[i][1] - this.circles[i][2] > this.height){
-				this.circles[i][0] = Math.random() * this.width;
-				this.circles[i][2] = Math.random() * 100;
-				this.circles[i][1] = 0 - this.circles[i][2];
-				this.circles[i][3] = Math.random() / 2;
+			if (this.circles[i].y - this.circles[i].radius > this.height){
+				this.circles[i].x = Math.random() * this.width;
+				this.circles[i].radius = Math.random() * 5;
+				this.circles[i].y = 0 - this.circles[i].radius;
+				this.circles[i].transparency = this.getTransparency();
 			}
 			else{
-				this.circles[i][1] += (deltaY + this.circles[i][4]);
+				this.circles[i].y += (deltaY + this.circles[i].speed);
 			}
 		}
 	}
+
+	this.getTransparency = function(){
+		return Math.random() / 4;
+	}
 };
+
+star = function(x, y, radius, transparency, speed){
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.transparency = transparency;
+	this.speed = speed;
+
+	this.draw = function(ctx){
+		ctx.fillStyle = 'rgba(255, 255, 0, ' + this.transparency + ')';
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+}
