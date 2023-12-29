1d6ead37088aba08bc5a7d78943e02f12fe73808
Jonathan <jonr79@hotmail.com>
2023-12-29 14:46:44 -0500

    Add LevelFactory

--- script/levelTracker.js
+++ script/levelTracker.js
@@ -4,15 +4,9 @@ var MakerState = {
 	NEXT_LEVEL:  2};
 
 function levelTracker(){
-	this.getLevel = function(){
-		let phases = [];
-		phases.push(new phase());
-		phases.push(new phase());
-		return new level(phases, this.levelNumber);
-	}
-
 	this.levelNumber = 1;
-	this.level = this.getLevel();
+	this.levelFactory = new levelFactory();
+	this.level = this.levelFactory.create(this.levelNumber);
 	this.state = MakerState.PRODUCE;
 	this.startProducingAt = 0;
 	
@@ -66,67 +60,10 @@ function levelTracker(){
 		this.state = MakerState.NEXT_LEVEL;
 		this.startProducingAt = frameNumber + 150;
 		this.levelNumber += 1;
-		this.level = this.getLevel();
+		this.level = this.levelFactory.create(this.levelNumber);
 	}
 
 	this.showLevelUp = function(){
 		return this.state == MakerState.NEXT_LEVEL;
-	}
-}
-
-function level(phases, levelNumber){
-	this.phases = phases;
-	this.currentPhase = 0;
-	this.number = levelNumber
-
-	this.nextAlien = function(){
-		return this.phases[this.currentPhase].nextAlien;
-	}
-
-	this.setNextAlien = function(frameNumber){
-		this.phases[this.currentPhase].setNextAlien(frameNumber);
-	}
-
-	this.incrementAliensProduced = function(){
-		this.phases[this.currentPhase].aliensProduced += 1;
-	}
-
-	this.producedAllAliens = function(){
-		return this.phases[this.currentPhase].producedAllAliens();
-	}
-
-	this.incrementPhase = function(){
-		if (this.currentPhase < this.phases.length -1)
-			this.currentPhase += 1;
-	}
-
-	this.levelComplete = function(){
-		return this.currentPhase == this.phases.length - 1 && this.phases[this.currentPhase].phaseComplete();
-	}
-
-	this.getEnemy = function(){
-		if (this.currentPhase % 2 == 0)
-			return new swooper((Math.random() * 300 + 100), Math.random() * 50, this.number, this.number);
-		else
-			return new demon((Math.random() * 350 + 50), Math.random() * 50, this.number, this.number);
-	}
-}
-
-function phase(){
-	this.aliensInPhase = 10;
-	this.interval = 100;
-	this.nextAlien = 0;
-	this.aliensProduced = 0;
-
-	this.setNextAlien = function(frameNumber){
-		this.nextAlien = frameNumber + this.interval;
-	}
-
-	this.producedAllAliens = function(){
-		return this.aliensProduced == this.aliensInPhase;
-	}
-
-	this.phaseComplete = function(){
-		return this.aliensProduced == this.aliensInPhase;
 	}
 }
