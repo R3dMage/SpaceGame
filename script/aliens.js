e8dc452a527b2ae6c3d78fa37ac22e76e9e8dd7d
Jonathan <jonr79@hotmail.com>
2024-01-02 08:26:19 -0500

    Unify to camel case

--- script/aliens.js
+++ script/aliens.js
@@ -11,21 +11,21 @@ var PredatorState = {
 function swooper(x, y, health, weight){
 	this.loc = new position(x, y, 40, 15);
 	this.readyToShoot = false;
-	this.Health = health;
-	this.Weight = weight;
-	this.State = SwooperState.SWOOP;
+	this.health = health;
+	this.weight = weight;
+	this.state = SwooperState.SWOOP;
 	this.moveRight = true;
-	this.Color = '8844FF'
-	let X = (Math.random() * 10) + 80;
-	this.PosX = this.loc.X + X;
-	this.NegX = this.loc.X - X;
-	this.OrigY = this.loc.Y;
-	this.OrigX = this.loc.X;
-	this.Swoop = 0;
-	this.SwoopMax = Math.random() * 10;
+	this.color = '8844FF'
+	let randomX = (Math.random() * 10) + 80;
+	this.posX = this.loc.X + randomX;
+	this.negX = this.loc.X - randomX;
+	this.origY = this.loc.Y;
+	this.origX = this.loc.X;
+	this.swoop = 0;
+	this.swoopMax = Math.random() * 10;
 
 	this.draw = function(ctx){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return;
 
 		if (debugCollisions){
@@ -36,7 +36,7 @@ function swooper(x, y, health, weight){
 
 		var drawX = this.loc.X + 25;
 		var drawY = this.loc.Y;
-		this.Color = WeightChart(this.Health);
+		this.color = WeightChart(this.health);
 
 		ctx.beginPath();
 		ctx.moveTo(drawX - 25, drawY);
@@ -44,7 +44,7 @@ function swooper(x, y, health, weight){
 		ctx.lineTo(drawX + 25, drawY);
 		ctx.lineTo(drawX - 25, drawY);
 		ctx.closePath();
-		ctx.fillStyle = this.Color;
+		ctx.fillStyle = this.color;
 		ctx.fill();
 	}
 
@@ -56,52 +56,52 @@ function swooper(x, y, health, weight){
 		if (this.isDead())
 			return;
 
-		switch(this.State){
+		switch(this.state){
 			case SwooperState.SWOOP:
 				if (this.moveRight){
-					if (this.loc.getX1() < this.PosX &&
+					if (this.loc.getX1() < this.posX &&
 						this.loc.getX1() < 500)
 						this.loc.X += 5;
 					else{
 						this.moveRight = false;
-						this.Swoop += 1;
+						this.swoop += 1;
 					}
 				}
 				else{
-					if (this.loc.X > this.NegX &&
+					if (this.loc.X > this.negX &&
 						this.loc.X > 10)
 						this.loc.X -= 5;
 					else{
 						this.moveRight = true;
-						this.Swoop += 1;
+						this.swoop += 1;
 					}
 				}
-				if(this.Swoop >= this.SwoopMax &&
-					this.loc.X > this.OrigX - 10 &&
-					this.loc.X < this.OrigX + 10){
-					this.Swoop = 0;
-					this.State = SwooperState.RUSH;
+				if(this.swoop >= this.swoopMax &&
+					this.loc.X > this.origX - 10 &&
+					this.loc.X < this.origX + 10){
+					this.swoop = 0;
+					this.state = SwooperState.RUSH;
 				}
 				break;
 			case SwooperState.RUSH:
 				this.loc.Y += 15;
-				if (this.loc.Y > this.OrigY + 300){
-					this.OrigY += 100;
+				if (this.loc.Y > this.origY + 300){
+					this.origY += 100;
 					if (this.loc.Y > 600)
-						this.OrigY = 50;
-					this.State = SwooperState.RETREAT;
+						this.origY = 50;
+					this.state = SwooperState.RETREAT;
 				}
 				break;
 			case SwooperState.RETREAT:
 				this.loc.Y -= 5;
-				if (this.loc.Y <= this.OrigY)
-					this.State = SwooperState.SWOOP;
+				if (this.loc.Y <= this.origY)
+					this.state = SwooperState.SWOOP;
 				break;
 			}
 	}
 
 	this.isDead = function(){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return true;
 		
 		return false;
@@ -110,8 +110,8 @@ function swooper(x, y, health, weight){
 
 function demon(x, y, health, weight) {
 	this.loc = new position(x, y, 20, 25);
-	this.Health = health;
-	this.Weight = weight;
+	this.health = health;
+	this.weight = weight;
 	this.readyToShoot = false;
 	this.isDead = false;
 	
@@ -138,7 +138,7 @@ function demon(x, y, health, weight) {
 	}
 
 	this.draw = function(ctx){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return;
 
 		if (debugCollisions){
@@ -147,7 +147,7 @@ function demon(x, y, health, weight) {
 			return;
 		}
 
-		this.Color = WeightChart(this.Health);
+		this.color = WeightChart(this.health);
 
 		ctx.beginPath();
 		ctx.moveTo(this.loc.X + this.loc.Width / 2, this.loc.Y);
@@ -155,7 +155,7 @@ function demon(x, y, health, weight) {
 		ctx.lineTo(this.loc.X + this.loc.Width / 2, this.loc.Y + this.loc.Height);
 		ctx.lineTo(this.loc.X, this.loc.Y + this.loc.Height / 2);
 		ctx.closePath();
-		ctx.fillStyle = this.Color;
+		ctx.fillStyle = this.color;
 		ctx.fill();
 	}
 
@@ -164,7 +164,7 @@ function demon(x, y, health, weight) {
 	}
 
 	this.isDead = function(){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return true;
 		
 		return false;
@@ -201,14 +201,14 @@ function demon(x, y, health, weight) {
 
 function predator (x, y, health, weight) {
 	this.loc = new position(x, y, 20, 25);
-	this.Health = health;
-	this.Weight = weight;
-	this.State = PredatorState.WAIT;
+	this.health = health;
+	this.weight = weight;
+	this.state = PredatorState.WAIT;
 
 	this.proximity = 25;	
-	let X = Math.random() * 500;
-	let Y = Math.random() * 100;	
-	this.destination = new position(X, Y, 0, 0);
+	let randomX = Math.random() * 500;
+	let randomY = Math.random() * 100;	
+	this.destination = new position(randomX, randomY, 0, 0);
 	this.incrementX = (this.destination.X - this.loc.X) / 50;
 	this.incrementY = (this.destination.Y - this.loc.Y) / 50;
 
@@ -219,19 +219,19 @@ function predator (x, y, health, weight) {
 		if (this.isDead())
 			return;
 
-		switch(this.State){
+		switch(this.state){
 			case PredatorState.SHOOT:
 				// this.shotWait += 1;
 				// if (this.shotWait >= this.waitTime){
 				// 	this.shotWait = 0;
-				// 	this.State = PredatorState.WAIT;
+				// 	this.state = PredatorState.WAIT;
 				// }
 				break;
 			case PredatorState.WAIT:
 				this.shotWait += 1;
 				if (this.shotWait >= this.waitTime){
 					this.shotWait = 0;
-					this.State = PredatorState.MOVE;
+					this.state = PredatorState.MOVE;
 				}
 				break;
 			case PredatorState.MOVE:
@@ -242,7 +242,7 @@ function predator (x, y, health, weight) {
 					this.destination = this.getDestination();
 					this.incrementX = (this.destination.X - this.loc.X) / 50;
 					this.incrementY = (this.destination.Y - this.loc.Y) / 50;
-					this.State = PredatorState.SHOOT;
+					this.state = PredatorState.SHOOT;
 					this.readyToShoot = true;
 				}
 				break;
@@ -250,7 +250,7 @@ function predator (x, y, health, weight) {
 	}
 
 	this.draw = function(ctx){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return;
 
 		if (debugCollisions){
@@ -259,7 +259,7 @@ function predator (x, y, health, weight) {
 			return;
 		}
 
-		this.Color = WeightChart(this.Health);
+		this.color = WeightChart(this.health);
 
 		ctx.beginPath();
 		ctx.moveTo(this.loc.centerX(), this.loc.Y);
@@ -267,26 +267,26 @@ function predator (x, y, health, weight) {
 		ctx.lineTo(this.loc.centerX(), this.loc.getY1());
 		ctx.lineTo(this.loc.X, this.loc.Y + this.loc.Height / 3);
 		ctx.closePath();
-		ctx.fillStyle = this.Color;
+		ctx.fillStyle = this.color;
 		ctx.fill();
 	}
 
 	this.isDead = function(){
-		if (this.Health <= 0)
+		if (this.health <= 0)
 			return true;
 		
 		return false;
 	}
 
 	this.canShoot = function(){
-		if (this.State == PredatorState.SHOOT){
+		if (this.state == PredatorState.SHOOT){
 			return true;
 		}
 		return false;
 	}
 
 	this.getProjectile = function(playerLoc){
-		this.State = PredatorState.WAIT;
+		this.state = PredatorState.WAIT;
 		let speed = 15;
 		let X = this.loc.centerX();
 		let Y = this.loc.centerY();
@@ -296,24 +296,24 @@ function predator (x, y, health, weight) {
 
 		let target = new position(playerLoc.X, playerLoc.Y, 0, 0)
 
-		return new missile(X + incrementX, Y + incrementY, target, speed, this.Weight);
+		return new missile(X + incrementX, Y + incrementY, target, speed, this.weight);
 	}
 
 	this.getDestination = function(){
 		let newDestination = this.destination;
 		while(this.distanceFromPoint(newDestination.X, newDestination.Y) < this.proximity){
-			let X = Math.random() * 500;
-		if (X < 10)
-			X = 10;
-		else if (X > 490)
-			X = 490;
-		let Y = Math.random() * 700;
-		if (Y < 10)
-			Y = 10;
-		else if (Y > 700)
-			Y = 700;
+			let randomX = Math.random() * 500;
+		if (randomX < 10)
+			randomX = 10;
+		else if (randomX > 490)
+			randomX = 490;
+		let randomY = Math.random() * 700;
+		if (randomY < 10)
+			randomY = 10;
+		else if (randomY > 700)
+			randomY = 700;
 
-		newDestination = new position(X, Y, 0, 0);
+		newDestination = new position(randomX, randomY, 0, 0);
 		}
 		
 		return newDestination;
